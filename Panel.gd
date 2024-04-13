a26469510dcda88dd2c2e5b93a15600ed4cbf1c3
KaarenTenson <tensonkaaren@gmail.com>
2024-04-13 21:54:42 +0300

    panel

--- Panel.gd
+++ Panel.gd
@@ -4,30 +4,44 @@ extends Panel
 var pos
 var punanepos;
 var kollanepos;
+var punanetekst
+var kollanetekst
+@onready var label = $Label
+var tektpos
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pos=texture_rect.position# Replace with function body.
+	tektpos=label.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-func lisapilt(path, pos1, mitu):
-	var uus=texture_rect.duplicate()
-	uus.set_position(pos1+Vector2(30*mitu,0))
-	var img = load(path)
-	uus.texture=img
-	pildid.add_child(uus)
+func muudatekst(tekst1, mitu):
+	tekst1.text=str(mitu)+"X"
 func lisa(mida, mitu):
 	if(mitu==1):
-		pos+=Vector2(0,50)
+		var img
+		var uus=texture_rect.duplicate()
+		var uustekst=label.duplicate()
+		uus.set_position(pos+Vector2(30*mitu,0))
+		uustekst.set_position(tektpos+Vector2(30*mitu,0))
 		if(mida=="punane"):
+			punanetekst=uustekst
 			punanepos=pos
+			img = load("res://assets/elf/elf_red_stand.png")
 		if(mida=="kollane"):
+			kollanetekst=uustekst
 			kollanepos=pos
+			img = load("res://assets/elf/elf_yellow_stand.png")
+		uus.texture=img
+		pos+=Vector2(0,50)
+		tektpos+=Vector2(0,50)
+		pildid.add_child(uus)
+		pildid.add_child(uustekst)
 	if(mida=="punane"):
-		lisapilt("res://assets/elf/elf_red_stand.png",punanepos, mitu)
+		muudatekst(punanetekst,mitu)
 	if(mida=="kollane"):
-		lisapilt("res://assets/elf/elf_yellow_stand.png",kollanepos, mitu)
+		muudatekst(kollanetekst,mitu)
