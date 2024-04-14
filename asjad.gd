1f540157b66faaf81b1de3d53bef51ac15a69a8c
KaarenTenson <tensonkaaren@gmail.com>
2024-04-14 13:31:12 +0300

    lisasin heli

--- asjad.gd
+++ asjad.gd
@@ -1,5 +1,5 @@
 extends Button
-
+@onready var audio_stream_player_2d = $AudioStreamPlayer2D
 @onready var main = $".."
 @onready var nupp = $"."
 var kaspeal=false
@@ -16,6 +16,7 @@ func _process(_delta):
 
 func _on_pressed():
 	if(!kaspeal):
+		audio_stream_player_2d.play()
 		kaspeal = true
 	else:
 		if(main.getpada()):
