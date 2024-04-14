b47558350cbc76554a89e8569d63710903057132
Gregor Artur <144439475+greeeegartur@users.noreply.github.com>
2024-04-14 12:22:36 +0300

    sauna judgment

--- stats.gd
+++ stats.gd
@@ -3,13 +3,10 @@ var päkkapikke = 0
 var kollaseidpäkkapikke = 0
 var siniseidpäkkapikke = 0
 var rohelisipäkkapikke = 0
-
-var rng = RandomNumberGenerator.new()
-
-var mesi = 0
+var mesi = 1
 var jook = 0
 var vesi = 0
-var lapinkulta = 0
+var lapinkulta = 1
 var viht1 = 0
 var viht2 = 0
 var viht3 = 1
@@ -20,8 +17,9 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
+	
 func muuda(punaseid,kollaseid, siniseid, rohelisi, mee, joo, vee, lapin, viht11, viht22, viht33):
 	päkkapikke = punaseid
 	kollaseidpäkkapikke = kollaseid
@@ -36,25 +34,31 @@ func muuda(punaseid,kollaseid, siniseid, rohelisi, mee, joo, vee, lapin, viht11,
 	viht3 = viht33
 
 func judge_õlu(õlu):
-	if õlu >= lapinkulta:
-		return true
+	if ((õlu <= lapinkulta) and (jook == 0)):
+		Dialogic.VAR.judgment.set('õlu', true)
 	else:
 		return false
 	
 func judge_viht(viht):
-	if viht >= viht3:
+	if ((viht <= viht3) and (viht2 == 0) and (viht1 == 0)):
 		Dialogic.VAR.judgment.set('viht', true)
 	else:
 		return false
 	
 func judge_päkapikk(päkapikk):
-	if päkapikk >= rohelisipäkkapikke:
-		return true
+	if (päkapikk <= rohelisipäkkapikke) and (päkkapikke == 0) and (kollaseidpäkkapikke) == 0 and (siniseidpäkkapikke == 0):
+		Dialogic.VAR.judgment.set('pakapikk', true)
 	else:
 		return false
 	
 func judge_mesi(judgemesi):
-	if judgemesi >= mesi:
-		return true
+	if judgemesi <= mesi:
+		Dialogic.VAR.judgment.set('judmesi', true)
+	else:
+		return false
+
+func judgment_final(tingimus):
+	if ((tingimus <= 0) and (Dialogic.VAR.judgment.get('õlu') == true) and (Dialogic.VAR.judgment.get('viht') == true) and (Dialogic.VAR.judgment.get('päkapikk') == true) and (Dialogic.VAR.judgment.get('mesi')) == true):
+		Dialogic.VAR.judgment.set('tingimus', true)
 	else:
 		return false
