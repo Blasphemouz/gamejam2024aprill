5a785dfc2425ac3cec6f759df7a50e3d8ea808ab
Gregor Artur <144439475+greeeegartur@users.noreply.github.com>
2024-04-14 15:46:30 +0300

    jyrki court

---
+++ pressenter.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var i = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _input(event):
+	if i == 0 and event is InputEventKey and event.keycode == KEY_ENTER and event.pressed:
+		i += 1
+		Dialogic.start('poliisi_sauna')
+		get_viewport().set_input_as_handled()
